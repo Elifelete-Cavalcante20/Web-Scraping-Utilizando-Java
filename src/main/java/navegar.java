dd77c0dc6c489404242636508daa970d0057ccdc
Elifelete Cavalcante <elif.cav00@gmail.com>
2025-05-12 21:45:38 -0300

    Agora a asemulaçao tela funcionona como planejado e os produtos sao devolvidos por nome no terminal

--- src/main/java/navegar.java
+++ src/main/java/navegar.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
 
 public class navegar {
@@ -12,11 +15,38 @@ public class navegar {
 
         navegador.get("https://www.mercadolivre.com.br/");
 
-        List<WebElement> section = navegador.findElements(By.cssSelector("div[class*='poly-card']"));
-        for (WebElement p : section) {
-            System.out.println(p.getText());
+        try {
+            Thread.sleep(1500);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        WebElement scroll = navegador.findElement(By.xpath("//*[@id=\"root-app\"]/div/div[1]/section[2]/div[2]/section/section/div/div/div/div[1]/h2"));
+        navegador.executeScript("arguments[0].scrollIntoView(true);", scroll);
+
+        boolean continua = true;
+        while (continua) {
+            try {
+                List<WebElement> section = navegador.findElements(By.xpath("//*[@id=\":R9ode:\"]/div[2]/div//a[contains(@class, 'poly-component__title')]"));
+                for (WebElement p : section) {
+                    System.out.println(p.getText());
+                }
+                WebElement botaoCarrosel = navegador.findElement(By.xpath("//*[@id=\":R9ode:\"]/div[2]/button[2]"));
+                if (botaoCarrosel.isEnabled()) {
+                    botaoCarrosel.click();
+                    Thread.sleep(1500);
+                } else {
+                    continua = false;
+                }
+            } catch (Exception e) {
+                continua = false;
+                System.err.println("Fim do carrosel ou erro ao encontrar próxima página.");
+                e.printStackTrace();
+            }
+        }
+        navegador.close();
     }
 }
+
 
 
